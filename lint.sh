716e1427066baca04ac70bf28fbe60e44b5c5d45
Leqiao Peng <LeqiaoP1@verifone.com>
2014-06-02 15:37:36 +0200

    assign00

--- lint.sh
+++ lint.sh
@@ -1,7 +1,10 @@
 export PYTHONPATH=./src/learn_python_the_hard_way:$PYTHONPATH
+export PYTHONPATH=./src/introduction_to_computer_science_and_programming:$PYTHONPATH
 
-flake8 --ignore=E501,E128,E701,E261,E301,E126,E127,E131 test/
-flake8 --ignore=E501,E128,E701,E261,E301,E126,E127,E131 src/
+flake8 --ignore=E501,E128,E701,E261,E301,E302,E126,E127,E131,W293 $1
+
+
+
 
 
 
