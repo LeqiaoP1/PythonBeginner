6bdcd43616b5faa5b8b8bada3043fca53bda32b5
Leqiao Peng <LeqiaoP1@verifone.com>
2014-06-12 16:07:29 +0200

    assn11 done

--- src/introduction_to_computer_science_and_programming/assn11/ps11.py
+++ src/introduction_to_computer_science_and_programming/assn11/ps11.py
@@ -5,6 +5,11 @@
 
 import math
 
+import random
+import pylab
+import ps11_visualize
+
+
 # === Provided classes
 
 class Position(object):
@@ -65,7 +70,11 @@ class RectangularRoom(object):
         width: an integer > 0
         height: an integer > 0
         """
-        # TODO: Your code goes here
+        self.height = height
+        self.width = width
+        self.dirty_rooms = [[x, y] for x in range(width) for y in range(height)]
+        self.clean_rooms = []
+
     def cleanTileAtPosition(self, pos):
         """
         Mark the tile under the position POS as cleaned.
@@ -73,7 +82,13 @@ class RectangularRoom(object):
 
         pos: a Position
         """
-        # TODO: Your code goes here
+        m = int(pos.getX())
+        n = int(pos.getY())
+        if not self.isTileCleaned(m, n):
+            self.dirty_rooms.remove([m, n])
+            self.clean_rooms.append([m, n])
+
+
     def isTileCleaned(self, m, n):
         """
         Return True if the tile (m, n) has been cleaned.
@@ -84,28 +99,35 @@ class RectangularRoom(object):
         n: an integer
         returns: True if (m, n) is cleaned, False otherwise
         """
-        # TODO: Your code goes here
+        return [m, n] in self.clean_rooms
+
     def getNumTiles(self):
         """
         Return the total number of tiles in the room.
 
         returns: an integer
         """
-        # TODO: Your code goes here
+        return self.height * self.width
+
     def getNumCleanedTiles(self):
         """
         Return the total number of clean tiles in the room.
 
         returns: an integer
         """
-        # TODO: Your code goes here
+        return len(self.clean_rooms)
+
     def getRandomPosition(self):
         """
         Return a random position inside the room.
 
         returns: a Position object.
         """
-        # TODO: Your code goes here
+        r_x = random.uniform(0, self.width)
+        r_y = random.uniform(0, self.height)
+
+        return Position(r_x, r_y)
+
     def isPositionInRoom(self, pos):
         """
         Return True if POS is inside the room.
@@ -113,7 +135,7 @@ class RectangularRoom(object):
         pos: a Position object.
         returns: True if POS is in the room, False otherwise.
         """
-        # TODO: Your code goes here
+        return (pos.getX() >= 0 and pos.getX() < self.width) and (pos.getY() >= 0 and pos.getY() < self.height)
 
 
 class BaseRobot(object):
@@ -141,14 +163,22 @@ class BaseRobot(object):
         room:  a RectangularRoom object.
         speed: a float (speed > 0)
         """
-        # TODO: Your code goes here
+        self.room = room
+        self.speed = float(speed)
+
+        self.position = self.room.getRandomPosition()
+        self.direction = int(random.uniform(0, 360))
+
+
     def getRobotPosition(self):
         """
         Return the position of the robot.
 
         returns: a Position object giving the robot's position.
         """
-        # TODO: Your code goes here
+        return self.position
+
+
     def getRobotDirection(self):
         """
         Return the direction of the robot.
@@ -156,21 +186,23 @@ class BaseRobot(object):
         returns: an integer d giving the direction of the robot as an angle in
         degrees, 0 <= d < 360.
         """
-        # TODO: Your code goes here
+        return self.direction
+
     def setRobotPosition(self, position):
         """
         Set the position of the robot to POSITION.
 
         position: a Position object.
         """
-        # TODO: Your code goes here
+        self.position = position        
+
     def setRobotDirection(self, direction):
         """
         Set the direction of the robot to DIRECTION.
 
         direction: integer representing an angle in degrees
         """
-        # TODO: Your code goes here
+        self.direction = direction
 
 
 class Robot(BaseRobot):
@@ -188,7 +220,15 @@ class Robot(BaseRobot):
         Move the robot to a new position and mark the tile it is on as having
         been cleaned.
         """
-        # TODO: Your code goes here
+        predict_pos = self.position.getNewPosition(self.direction, self.speed)
+
+        # if not in-room, then regenerate the direction and predict next position
+        while not self.room.isPositionInRoom(predict_pos):
+            self.direction = int(random.uniform(0, 360))
+            predict_pos = self.position.getNewPosition(self.direction, self.speed)
+        
+        self.position = predict_pos # update with an in-room position
+        self.room.cleanTileAtPosition(self.position)
 
 
 # === Problem 3
@@ -217,7 +257,31 @@ def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
                 RandomWalkRobot)
     visualize: a boolean (True to turn on visualization)
     """
-    # TODO: Your code goes here
+
+    trial_records = [[] for t_id in range(num_trials)]
+    
+    for t_id in range(num_trials):
+        if visualize:
+            anim = ps11_visualize.RobotVisualization(num_robots, width, height, 1)
+        # reinitil Room and Robots for each new trial
+        room = RectangularRoom(width, height)
+        robots = [robot_type(room, speed) for r in range(num_robots)]
+        coverage = 0.0
+        while (coverage < min_coverage):
+            for r in robots:
+                r.updatePositionAndClean()
+                
+            if visualize:
+                anim.update(room, robots)
+            coverage = (1.0 * room.getNumCleanedTiles() / room.getNumTiles())
+            trial_records[t_id].append(coverage)
+
+        if visualize:
+            anim.done()
+
+    return trial_records
+
+
 
 # === Provided function
 def computeMeans(list_of_lists):
@@ -250,31 +314,98 @@ def computeMeans(list_of_lists):
 
 
 # === Problem 4
+def p4_helper(list_of_lists):
+    """ average length of all nested lists """
+    number_trials = len(list_of_lists)
+    assert( not(number_trials == 0) )
+    l_sum = sum( [len(list_of_lists[i]) for i in range(number_trials)] )
+    return l_sum/number_trials
+    
 def showPlot1():
     """
     Produces a plot showing dependence of cleaning time on room size.
     """
-    # TODO: Your code goes here
+    rooms = [[5, 5], [10,10], [15,15], [20,20], [25,25]]
+    x_data = []
+    y_data = []
+    for room in rooms:
+        x_data.append( room[0] * room[1] )
+        list_steps = runSimulation(1, 1.0, room[0], room[1], 0.75, 10, Robot, False)
+        
+        y_data.append( p4_helper(list_steps) )
+
+    pylab.plot(x_data, y_data)
+    pylab.title('dependence on sizes of room with single robot(speed = 1.0)')
+    pylab.xlabel('room size')
+    pylab.ylabel('steps needed to clean 75%')
+    pylab.show()
+
 
 def showPlot2():
     """
     Produces a plot showing dependence of cleaning time on number of robots.
     """
-    # TODO: Your code goes here
+    number_of_robots = range(1, 11)
+    x_data = number_of_robots
+    y_data = []
+    for nr_rb in number_of_robots:
+        list_steps = runSimulation(nr_rb, 1.0, 25, 25, 0.75, 10, Robot, False)
+        y_data.append( p4_helper(list_steps) )
+
+    pylab.plot(x_data, y_data)
+    pylab.title('dependence on number of robots(speed:1.0)')
+    pylab.xlabel('number of robots in room (size: 25*25)')
+    pylab.ylabel('steps needed to clean 75%')
+    pylab.show()
+    
 
 def showPlot3():
     """
     Produces a plot showing dependence of cleaning time on room shape.
     """
-    # TODO: Your code goes here
+    rooms = [[20, 20], [25, 16], [40, 10], [50, 8], [8, 50], [100, 4]]
+    
+    x_data = []
+    y_data = []
+    for room in rooms:
+        x_data.append( room[0]*1.0/room[1])
+        list_steps = runSimulation(2, 1.0, room[0], room[1], 0.75, 20, Robot, False)       
+        y_data.append( p4_helper(list_steps) )
 
+    # resort the ratio
+    sorted_x = sorted(x_data)
+    sorted_index = sorted(range(len(x_data)), key=lambda x:x_data[x])
+    sorted_y = [y_data[i] for i in sorted_index]
+    pylab.plot(sorted_x, sorted_y)
+    pylab.title('dependence on W/H-ratio of room with two robots(speed = 1.0)')
+    pylab.xlabel('W/H Ratio of rooms with same size: 400')
+    pylab.ylabel('steps needed to clean 75%')
+    pylab.show()
+
+
+from matplotlib import pyplot as plt
 def showPlot4():
     """
     Produces a plot showing cleaning time vs. percentage cleaned, for
     each of 1-5 robots.
     """
-    # TODO: Your code goes here
+    coverages = [x * 0.1 for x in range(1, 10)]
+    
+    for nr_robots in [1, 2, 3, 4, 5]:
+        y_steps = []
+        for cov in coverages:
+            trial_steps = runSimulation(nr_robots, 1.0, 25, 25, cov, 10, Robot, False)
+            y_steps.append( p4_helper(trial_steps) )
 
+    
+        plt.plot(coverages, y_steps, label="nr:%d"%(nr_robots))
+    
+
+    pylab.title('dependence on requried coverage of cleaned tiles')
+    pylab.xlabel('required coverages in the room(size: 25*25)')
+    pylab.ylabel('steps needed')
+    plt.legend()
+    plt.show()
 
 # === Problem 5
 
@@ -284,13 +415,52 @@ class RandomWalkRobot(BaseRobot):
     strategy: it chooses a new direction at random after each
     time-step.
     """
-    # TODO: Your code goes here
+    def updatePositionAndClean(self):
+        """
+        Simulate the passage of a single time-step.
 
+        Move the robot to a new position and mark the tile it is on as having
+        been cleaned.
+        """
+        self.room.cleanTileAtPosition(self.position)
 
+        self.direction = int(random.uniform(0, 360))
+        predict_pos = self.position.getNewPosition(self.direction, self.speed)
+        
+        # if not in-room, then regenerate the direction and predict next position
+        while not self.room.isPositionInRoom(predict_pos):
+            self.direction = int(random.uniform(0, 360))
+            predict_pos = self.position.getNewPosition(self.direction, self.speed)
+        
+        self.position = predict_pos # update with an in-room position
+
+        
 # === Problem 6
 
 def showPlot5():
     """
     Produces a plot comparing the two robot strategies.
     """
-    # TODO: Your code goes here
+    coverages = [x * 0.1 for x in range(2, 9)]
+
+    # for standard robot
+    y_steps = []
+    for cov in coverages:
+        trial_steps = runSimulation(1, 1.0, 20, 20, cov, 20, Robot, False)
+        y_steps.append( p4_helper(trial_steps) )
+        
+    plt.plot(coverages, y_steps, label="Standard Walk")
+
+    # for new randomwalk robot
+    y_steps = []
+    for cov in coverages:
+        trial_steps = runSimulation(1, 1.0, 20, 20, cov, 20, RandomWalkRobot, False)
+        y_steps.append( p4_helper(trial_steps) )
+        
+    plt.plot(coverages, y_steps, label="Random Walk")
+
+    pylab.title('dependence on requried coverage of cleaned tiles')
+    pylab.xlabel('required coverages in the room(size:20x20)')
+    pylab.ylabel('steps needed by one robot(speed:1.0)')
+    plt.legend()
+    plt.show()
